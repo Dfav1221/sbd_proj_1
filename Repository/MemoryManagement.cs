e6753a8b4b54bcf49d4aed81e7dfed14c8e45333
Jhirsz <jakub.hirsz2000@gmail.com>
2021-11-09 10:32:12 +0100

    new file

---
+++ Repository/MemoryManagement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Repository
+{
+    public class MemoryManagement
+    {
+        private Counter _counter;
+
+        private string _filePath;
+
+
+        public MemoryManagement(Counter counter, string filePath)
+        {
+            _counter = counter;
+            _filePath = filePath;
+        }
+
+        public double ReadNextRecord(TextReader reader)
+        {
+                var line = reader.ReadLine();
+                var numbersAsStrings = line.Split(' ').AsEnumerable();
+                var numbers = numbersAsStrings.Select(int.Parse);
+                var geometricMean = numbers.GeometricMean();
+                return geometricMean;
+        }
+    }
+}

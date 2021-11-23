d0d083f2bf8cd54f65f307389e85d71b48e28972
JakubHirsz <jakub.hirsz2000@gmail.com>
2021-11-23 22:24:56 +0100

    Base structure of project

---
+++ ConsoleUi/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsoleUi;
+
+public static class Program
+{
+
+    public static void Main(string[] args)
+    {
+    }
+}
+
